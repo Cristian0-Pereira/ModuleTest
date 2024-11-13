b783d184f39f8767b52ca7d04181a9401b74012e
Cristian0-Pereira <cristianops2020@gmail.com>
2024-11-13 17:47:24 -0300

    add

---
+++ CalculatorTest/ValidationsStringTester.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Calculator.Services;
+
+namespace CalculatorTest
+{
+    public class ValidationsStringTester
+    {
+        private ValidationsString _validations;
+
+        public ValidationsStringTester()
+        {
+            _validations = new ValidationsString();
+        }
+
+        public void Count3CharactersAndReturn3()
+        {
+            
+        }
+    }
+}
