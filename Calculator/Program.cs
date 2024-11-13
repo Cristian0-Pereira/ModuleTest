a510622df0c4148690e8db46e1043342063239ed
Cristian0-Pereira <cristianops2020@gmail.com>
2024-11-13 01:22:26 -0300

    test

---
+++ Calculator/Program.cs
@@ -0,0 +1,8 @@
+﻿using Calculator.Services;
+
+CalculatorImp c = new();
+
+int n1 = 5;
+int n2 = 10;
+
+Console.WriteLine($"{n1} + {n2} = {c.Add(n1, n2)}");
---
+++ Calculator/Services/CalculatorImp.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Calculator.Services
+{
+    public class CalculatorImp
+    {
+        public int Add(int n1, int n2)
+        {
+            return n1 + n2;
+        }
+    }
+}
