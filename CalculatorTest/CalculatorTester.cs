645461c92b530a38e27b403363e25e7e5d6f7678
Cristian0-Pereira <cristianops2020@gmail.com>
2024-11-13 17:27:49 -0300

    Add

--- CalculatorTest/CalculatorTester.cs
+++ CalculatorTest/CalculatorTester.cs
@@ -18,10 +18,20 @@ public class CalculatorTester
         int n1 = 5;
         int n2 = 10;
         int resultadoEsperado = 15;
-
         // Act
         int resultado = _calc.Add(n1, n2);
-
+        // Assert
+        Assert.Equal(resultadoEsperado, resultado);
+    }
+        [Fact]
+    public void DeveSomar10com10ERetornar20()
+    {
+        // Arrange
+        int n1 = 10;
+        int n2 = 10;
+        int resultadoEsperado = 20;
+        // Act
+        int resultado = _calc.Add(n1, n2);
         // Assert
         Assert.Equal(resultadoEsperado, resultado);
     }
